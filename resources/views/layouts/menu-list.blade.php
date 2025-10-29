1b3753e10535a4a66e206802d93b15c4c7cbb21e
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-10-29 12:41:31 +0100

    add all my files and update my composer

---
+++ resources/views/layouts/menu-list.blade.php
@@ -0,0 +1,138 @@
+<li class="pc-item pc-caption">
+  <label data-i18n="Navigation">Navigation</label>
+  <i class="ph-duotone ph-gauge"></i>
+</li>
+<li class="pc-item pc-hasmenu">
+  <a href="#!" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-gauge"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Dashboard">Dashboard</span>
+    <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+    <span class="pc-badge">2</span>
+  </a>
+  <ul class="pc-submenu">
+    <li class="pc-item"><a class="pc-link" href="/index" data-i18n="Analytics">Analytics</a></li>
+  </ul>
+</li>
+<li class="pc-item pc-hasmenu">
+  <a href="#!" class="pc-link"><span class="pc-micon"> <i class="ph-duotone ph-layout"></i></span><span class="pc-mtext" data-i18n="Layouts">Layouts</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+  <ul class="pc-submenu">
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-horizontal" data-i18n="Horizontal">Horizontal</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-vertical" data-i18n="Vertical">Vertical</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-vertical-tab" data-i18n="Vertical + Tab">Vertical + Tab</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-tab" data-i18n="Tab">Tab</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-2-column" data-i18n="2 Column">2 Column</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-big-compact" data-i18n="Big Compact">Big Compact</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-compact" data-i18n="Compact">Compact</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-moduler" data-i18n="Moduler">Moduler</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-creative" data-i18n="Creative">Creative</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-detached" data-i18n="Detached">Detached</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-advanced" data-i18n="Advanced">Advanced</a></li>
+    <li class="pc-item"><a class="pc-link" href="/demo/layout-extended" data-i18n="Extended">Extended</a></li>
+  </ul>
+</li>
+<li class="pc-item pc-caption">
+  <label data-i18n="Pages">Pages</label>
+  <i class="ph-duotone ph-devices"></i>
+</li>
+<li class="pc-item pc-hasmenu">
+  <a href="#!" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-shield-checkered"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Authentication">Authentication</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+  <ul class="pc-submenu">
+    <li class="pc-item pc-hasmenu">
+      <a href="#!" class="pc-link">
+        <span data-i18n="Authentication 1">Authentication 1</span>
+        <span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+      <ul class="pc-submenu">
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/login-v1" data-i18n="Login">Login</a></li>
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/register-v1" data-i18n="Register">Register</a></li>
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/forgot-password-v1" data-i18n="Forget Password">Forgot Password</a></li>
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/reset-password-v1" data-i18n="Reset Password">reset password</a> </li>
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/code-verification-v1" data-i18n="Code Verification">code verification</a></li>
+      </ul>
+    </li>
+    <li class="pc-item pc-hasmenu">
+      <a href="#!" class="pc-link">
+        <span data-i18n="Authentication 2">Authentication 2</span>
+        <span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+      <ul class="pc-submenu">
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/login-v2" data-i18n="Login">Login</a></li>
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/register-v2" data-i18n="Register">Register</a></li>
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/forgot-password-v2" data-i18n="Forget password">Forgot password</a> </li>
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/reset-password-v2" data-i18n="Reset password">reset password</a> </li>
+        <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/code-verification-v2" data-i18n="Code Verification">code verification</a></li>
+      </ul>
+    </li>
+    <li class="pc-item"><a class="pc-link" href="/pages/login-modal" data-i18n="Login Modal">Login Modal</a></li>
+  </ul>
+</li>
+<li class="pc-item pc-hasmenu">
+  <a href="#!" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-wrench"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Maintenance">Maintenance</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+  </a>
+  <ul class="pc-submenu">
+    <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/error-404" data-i18n="Error 404">Error 404</a></li>
+    <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/connection-lost" data-i18n="Connection lost">Connection lost</a></li>
+    <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/under-construction" data-i18n="Under construction">Under Construction</a></li>
+    <li class="pc-item"><a class="pc-link" target="_blank" href="/pages/coming-soon" data-i18n="Coming-Soon">Coming soon</a></li>
+  </ul>
+</li>
+<li class="pc-item pc-caption">
+  <label data-i18n="Other">Other</label>
+  <i class="ph-duotone ph-suitcase"></i>
+</li>
+<li class="pc-item pc-hasmenu">
+  <a href="#!" class="pc-link"><span class="pc-micon"> <i class="ph-duotone ph-tree-structure"></i> </span><span class="pc-mtext" data-i18n="Menu levels">Menu levels</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+  <ul class="pc-submenu">
+    <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 2.1">Level 2.1</a></li>
+    <li class="pc-item pc-hasmenu">
+      <a href="#!" class="pc-link">
+        <span data-i18n="Level 2.2">Level 2.2</span>
+        <span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+      <ul class="pc-submenu">
+        <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 3.1">Level 3.1</a></li>
+        <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 3.2">Level 3.2</a></li>
+        <li class="pc-item pc-hasmenu">
+          <a href="#!" class="pc-link">
+            <span data-i18n="Level 3.3">Level 3.3</span>
+            <span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+          <ul class="pc-submenu">
+            <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 4.1">Level 4.1</a></li>
+            <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 4.2">Level 4.2</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li class="pc-item pc-hasmenu">
+      <a href="#!" class="pc-link">
+        <span data-i18n="Level 2.3">Level 2.3</span>
+        <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+      </a>
+      <ul class="pc-submenu">
+        <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 3.1">Level 3.1</a></li>
+        <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 3.2">Level 3.2</a></li>
+        <li class="pc-item pc-hasmenu">
+          <a href="#!" class="pc-link">
+            <span data-i18n="Level 3.3">Level 3.3</span>
+            <span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
+          <ul class="pc-submenu">
+            <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 4.1">Level 4.1</a></li>
+            <li class="pc-item"><a class="pc-link" href="#!" data-i18n="Level 4.2">Level 4.2</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</li>
+<li class="pc-item"><a href="/other/sample-page" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-desktop"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Sample Page">Sample page</span></a></li>
