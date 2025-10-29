b802c56d64d6875ed36d33f2387275f070310f6b
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-10-29 16:36:53 +0100

    add backoffice controller

---
+++ app/Http/Controllers/backoffice/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\backoffice;
+
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        // make sure this file exists:
+        // resources/views/backoffice/index.blade.php
+        return view('backoffice.index');
+    }
+
+    public function pageView($routeName, $name = null)
+    {
+        // this will try to load resources/views/backoffice/pages/{routeName}.blade.php
+        return view("backoffice.pages.$routeName", ['name' => $name]);
+    }
+}
