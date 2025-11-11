bc88d5a4f2cf323f2d2f13b119287d23240fbe18
rochdi karouali <rochdi.karouali1234@gmail.com>
2025-11-11 18:42:39 +0100

    update my routes

--- routes/frontoffice.php
+++ routes/frontoffice.php
@@ -2,20 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontoffice\HomeController;
+use App\Http\Controllers\Frontoffice\PageController;
 
 // ===============================
 //  FRONT OFFICE – Public Routes
 //  Language: English only (for now)
 // ===============================
 
-// All public pages
 Route::middleware(['web'])->group(function () {
-    
+
     // Home Page
     Route::get('/', [HomeController::class, 'index'])->name('front.home');
-    
+
     // About Page
     Route::get('/about', [HomeController::class, 'about'])->name('front.about');
 
-    
+    // FAQ Page
+    Route::get('/faq', [PageController::class, 'faq'])->name('front.faq');
+
+    Route::get('/contact', [PageController::class, 'contact'])->name('front.contact');
 });
