28dded6e583eb974fc09802499848c0de1c14914
rochdi karouali <rochdi.karouali1234@gmail.com>
2025-11-12 18:10:59 +0100

    update my PageController add intensive page & oursites

--- app/Http/Controllers/frontoffice/PageController.php
+++ app/Http/Controllers/frontoffice/PageController.php
@@ -14,9 +14,31 @@ class PageController extends Controller
         // Loads resources/views/frontoffice/faq.blade.php
         return view('frontoffice.faq');
     }
+
+    /**
+     * Display the Contact page.
+     */
     public function contact()
     {
-        // Loads resources/views/frontoffice/faq.blade.php
+        // Loads resources/views/frontoffice/contact.blade.php
         return view('frontoffice.contact');
+    }
+
+    /**
+     * Display the Sites page.
+     */
+    public function sites()
+    {
+        // Loads resources/views/frontoffice/sites.blade.php
+        return view('frontoffice.sites');
+    }
+
+    /**
+     * Display the Intensive Courses page.
+     */
+    public function intensiveCourses()
+    {
+        // Loads resources/views/frontoffice/intensive-courses.blade.php
+        return view('frontoffice.intensive-courses');
     }
 }
