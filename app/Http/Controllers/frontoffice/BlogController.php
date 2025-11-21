a78dbed344cf7451cab3fb2c74b14901a255f2aa
rochdi karouali <rochdi.karouali1234@gmail.com>
2025-11-21 18:11:30 +0100

    update my database

--- app/Http/Controllers/frontoffice/BlogController.php
+++ app/Http/Controllers/frontoffice/BlogController.php
@@ -8,22 +8,11 @@ use App\Models\Category;
 
 class BlogController extends Controller
 {
-    /**
-     * BLOG LIST PAGE
-     * /blog
-     */
     public function index()
     {
-        // Latest posts for the main grid
         $posts = Post::latest()->paginate(9);
-
-        // Featured post (or first post)
         $featured = Post::latest()->first();
-
-        // Sidebar categories
         $categories = Category::orderBy('name')->get();
-
-        // Popular or random posts for sidebar
         $popular = Post::orderBy('views', 'desc')->take(5)->get();
 
         return view('frontoffice.blog.blog', [
@@ -34,24 +23,12 @@ class BlogController extends Controller
         ]);
     }
 
-
-
-    /**
-     * BLOG DETAILS PAGE
-     * /blog/{slug}
-     */
     public function details($slug)
     {
-        // Find article
         $post = Post::where('slug', $slug)->firstOrFail();
-
-        // Increment views (optional)
         $post->increment('views');
 
-        // Recent posts for sidebar
         $recentPosts = Post::latest()->take(5)->get();
-
-        // Categories for sidebar
         $categories = Category::orderBy('name')->get();
 
         return view('frontoffice.blog.blog-details', [
