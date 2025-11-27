bd0e0fc97fc6a37ed6969c8c268473f19338b02a
rochdi karouali <rochdi.karouali1234@gmail.com>
2025-11-27 17:17:26 +0100

    update group table

--- database/migrations/2025_10_29_115424_create_groups_table.php
+++ database/migrations/2025_10_29_115424_create_groups_table.php
@@ -5,7 +5,8 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up(): void {
+    public function up(): void
+    {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
 
@@ -13,13 +14,13 @@ return new class extends Migration {
             $table->foreignId('teacher_id')->constrained('teachers')->cascadeOnDelete();
 
             // Example: A1 – A2 – B1 – B2
-            $table->enum('level', ['A1', 'A2', 'B1', 'B2']); 
+            $table->enum('level', ['A1', 'A2', 'B1', 'B2']);
 
             // Example: "Morning Groups"
-            $table->string('period_label'); 
+            $table->string('period_label');
 
             // Example: "10:00 – 12:30"
-            $table->string('time_range');   
+            $table->string('time_range');
 
             $table->longText('description')->nullable();
 
@@ -30,7 +31,8 @@ return new class extends Migration {
         });
     }
 
-    public function down(): void {
+    public function down(): void
+    {
         Schema::dropIfExists('groups');
     }
 };
