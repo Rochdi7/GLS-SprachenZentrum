86169fcab114f10379b1750c850b5286a23c4cee
rochdi karouali <rochdi.karouali1234@gmail.com>
2025-12-05 14:14:38 +0100

    update my routes

--- routes/frontoffice.php
+++ routes/frontoffice.php
@@ -1,60 +1,85 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Frontoffice\HomeController;
 use App\Http\Controllers\Frontoffice\PageController;
+use App\Http\Controllers\Frontoffice\GlsController;
+use App\Http\Controllers\Frontoffice\GroupController;
+use App\Http\Controllers\Frontoffice\BlogController;
 
-Route::middleware(['web'])->group(function () {
 
-    // ============================
-    // HOME
-    // ============================
-    Route::get('/', [HomeController::class, 'index'])->name('front.home');
+// ============================
+// HOME
+// ============================
+Route::get('/', [HomeController::class, 'index'])->name('front.home');
 
-    // ============================
-    // ABOUT
-    // ============================
-    Route::get('/about', [HomeController::class, 'about'])->name('front.about');
 
-    // ============================
-    // FAQ + CONTACT
-    // ============================
-    Route::get('/faq', [PageController::class, 'faq'])->name('front.faq');
-    Route::get('/contact', [PageController::class, 'contact'])->name('front.contact');
+// ============================
+// ABOUT
+// ============================
+Route::get('/about', [HomeController::class, 'about'])->name('front.about');
 
-    // ============================
-    // SITES (Centers)
-    // ============================
-    Route::get('/sites', [PageController::class, 'sites'])->name('front.sites');
 
-    // Individual center pages
-    Route::get('/sites/rabat', [PageController::class, 'siteRabat'])->name('front.sites.rabat');
-    Route::get('/sites/sale', [PageController::class, 'siteSale'])->name('front.sites.sale');
-    Route::get('/sites/kenitra', [PageController::class, 'siteKenitra'])->name('front.sites.kenitra');
-    Route::get('/sites/casablanca', [PageController::class, 'siteCasablanca'])->name('front.sites.casablanca');
-    Route::get('/sites/agadir', [PageController::class, 'siteAgadir'])->name('front.sites.agadir');
-    Route::get('/sites/marrakech', [PageController::class, 'siteMarrakech'])->name('front.sites.marrakech');
+// ============================
+// FAQ + CONTACT
+// ============================
+Route::get('/faq', [PageController::class, 'faq'])->name('front.faq');
+Route::get('/contact', [PageController::class, 'contact'])->name('front.contact');
+Route::post('/contact', [PageController::class, 'contactPost'])->name('front.contact.post');
 
-    // ============================
-    // COURSES
-    // ============================
-    Route::get('/intensive-courses', [PageController::class, 'intensiveCourses'])->name('front.intensive-courses');
-    Route::get('/online-courses', [PageController::class, 'onlineCourses'])->name('front.online-courses');
-    Route::get('/pricing', [PageController::class, 'pricing'])->name('front.pricing');
 
-    // ============================
-    // EXAMS
-    // ============================
-    Route::get('/exams/gls', [PageController::class, 'glsExams'])->name('front.exams.gls');
-    Route::get('/exams/osd', [PageController::class, 'osdExams'])->name('front.exams.osd');
+// ============================
+// SITES
+// ============================
+Route::get('/sites/{slug}', [GroupController::class, 'show'])->name('front.sites.show');
 
-    // ============================
-    // RESOURCES
-    // ============================
-    Route::get('/blog', [PageController::class, 'blog'])->name('front.blog');
-    Route::get('/blog/test-details', function () {
-    return view('frontoffice.blog.blog-details-test');
-})->name('front.blog.details.test');
 
-    Route::get('/student-stories', [PageController::class, 'studentStories'])->name('front.student-stories');
-});
+// ============================
+// COURSES
+// ============================
+Route::get('/intensive-courses', [PageController::class, 'intensiveCourses'])->name('front.intensive-courses');
+Route::get('/online-courses', [PageController::class, 'onlineCourses'])->name('front.online-courses');
+Route::get('/pricing', [PageController::class, 'pricing'])->name('front.pricing');
+
+
+// ============================
+// EXAMS
+// ============================
+Route::get('/exams/gls', [PageController::class, 'glsExams'])->name('front.exams.gls');
+Route::get('/exams/osd', [PageController::class, 'osdExams'])->name('front.exams.osd');
+
+
+// ============================
+// BLOG (FRONT)
+// ============================
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+Route::get('/blog/{slug}', [BlogController::class, 'details'])->name('blog.show');
+
+
+// ============================
+// STUDENT STORIES
+// ============================
+Route::get('/student-stories', [PageController::class, 'studentStories'])->name('front.student-stories');
+
+
+// ============================
+// CERTIFICATE CHECK
+// ============================
+Route::get('/certificate-check', [PageController::class, 'certificateCheck'])->name('front.certificate.check');
+Route::post('/certificate-check', [PageController::class, 'certificateCheckPost'])->name('front.certificate.check.post');
+
+
+// ============================
+// NIVEAUX
+// ============================
+Route::get('/niveaux/a1', [PageController::class, 'niveauA1'])->name('front.niveaux.a1');
+Route::get('/niveaux/a2', [PageController::class, 'niveauA2'])->name('front.niveaux.a2');
+Route::get('/niveaux/b1', [PageController::class, 'niveauB1'])->name('front.niveaux.b1');
+Route::get('/niveaux/b2', [PageController::class, 'niveauB2'])->name('front.niveaux.b2');
+
+
+// ============================
+// FORM SUBMISSION
+// ============================
+Route::post('/gls-inscription', [GlsController::class, 'store'])->name('gls.inscription');
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,49 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-Route::middleware(['web'])->group(function () {
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-    Route::get('/media/{id}/{filename}', function ($id, $filename) {
-        $media = \Spatie\MediaLibrary\MediaCollections\Models\Media::findOrFail($id);
-        return response()->file($media->getPath());
-    })->name('media.custom');
+/**
+ * =============================
+ * AUTH ROUTES (NO LOCALE)
+ * =============================
+ */
+Auth::routes(['verify' => true]);
 
-    // Auth routes
-    Auth::routes(['verify' => true]);
+/**
+ * =============================
+ * MEDIA (NO LOCALE)
+ * =============================
+ */
+Route::get('/media/{id}/{filename}', function ($id, $filename) {
+    $media = \Spatie\MediaLibrary\MediaCollections\Models\Media::findOrFail($id);
+    return response()->file($media->getPath());
+})->name('media.custom');
 
-    // Backoffice
-    Route::middleware(['auth'])->group(function () {
-        require __DIR__.'/backoffice.php';
-    });
-
-    // Frontoffice
-    require __DIR__.'/frontoffice.php';
+/**
+ * =============================
+ * BACKOFFICE (NO LOCALE)
+ * =============================
+ */
+Route::middleware(['auth'])->group(function () {
+    require __DIR__ . '/backoffice.php';
 });
 
+/**
+ * =============================
+ * FRONT-END LOCALIZED (FR/EN)
+ * =============================
+ */
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [
+        'localize',
+        'localeSessionRedirect',
+        'localizationRedirect',
+        'localeViewPath',
+    ],
+], function () {
+
+    require __DIR__ . '/frontoffice.php';
+
+});
